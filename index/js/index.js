ad027bb3df3d471aacf7ee0308374d98264d9f1c
web-portfolio <nasko.1806@gmail.com>
2019-01-15 09:41:53 +0200

    Update index.js

--- index/js/index.js
+++ index/js/index.js
@@ -1,42 +1,41 @@
-document.addEventListener( 'DOMContentLoaded', function( event ) {
-  $(window).on("load resize", function(){
+document.addEventListener('DOMContentLoaded', function(event) {
+  $(window).on("load resize", function() {
     gridResize();
     getMobileOperatingSystem();
   });
 
-  function gridResize(){
+  function gridResize() {
     var w = $(".container > div").width();
-    var h = (w*56.2060889929742)/100;
+    var h = (w * 56.2060889929742) / 100;
     $(".container > div").height(h);
   }
 
   function getMobileOperatingSystem() {
-  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  if (/windows phone/i.test(userAgent)) {
-    $('link[href="index/css/hovers.css"]').prop('disabled', true);
-    return "Windows Phone";
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    var winPhone = /windows phone/i.test(userAgent);
+    var ios = /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream;
+    var android = /android/i.test(userAgent);
+
+    if (winPhone || ios || android) {
+      $('link[href="index/css/hovers.css"]').prop('disabled', true);
+      alert("phone");
+      return "Not Desktop Device";
+    } else {
+      $('link[href="index/css/hovers.css"]').prop('disabled', false);
+      $(".container > div")
+        .mouseover(function() {
+          $(this).find("div").show();
+        })
+        .mouseout(function() {
+          $(this).find("div").hide();
+        });
+      alert("win");
+      return "Desktop";
+    }
   }
-  if (/android/i.test(userAgent)) {
-    $('link[href="index/css/hovers.css"]').prop('disabled', true);
-    return "Android";
-  }
-  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    $('link[href="index/css/hovers.css"]').prop('disabled', true);
-    return "iOS";
-  }
-    $('link[href="index/css/hovers.css"]').prop('disabled', false);
-    $(".container > div")
-    .mouseover(function() {
-      $(this).find("div").show();
-    })
-    .mouseout(function() {
-      $(this).find("div").hide();
-    });
-  return "Desktop";
-}
 
   // JS LINKS
   $(".wow").click(function() {
-  window.location = "wow/home-page.html";
+    window.location = "wow/home-page.html";
   });
 });
