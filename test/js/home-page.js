faa20d9c29cc8c378452fd524f6fdebda534e417
web-portfolio <46299764+web-portfolio@users.noreply.github.com>
2019-01-25 19:49:20 +0200

    Update home-page.js

--- test/js/home-page.js
+++ test/js/home-page.js
@@ -9,8 +9,8 @@ document.addEventListener( 'DOMContentLoaded', function( event ) {
 
   $(window).on("load", function() {
     $(".header").load("header.html .container > *");
-    // $("head").append('<link rel="stylesheet" type="text/css" href="css/header.css">');
-    ('<link rel="stylesheet" type="text/css" href="css/media.css">').append('<link rel="stylesheet" type="text/css" href="css/header.css">');
+    $("head").append('<link rel="stylesheet" type="text/css" href="css/header.css">');
+    // ('<link rel="stylesheet" type="text/css" href="css/media.css">').append('<link rel="stylesheet" type="text/css" href="css/header.css">');
     
     $.getScript("js/custom-scrollbar.js");
     $.getScript("js/header.js");    
