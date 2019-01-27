0559f37ebcaca595367d46d99f451570f9a20740
web-portfolio <nasko.1806@gmail.com>
2019-01-27 21:32:52 +0200

    Update home-page.js

--- test/js/home-page.js
+++ test/js/home-page.js
@@ -10,7 +10,7 @@ document.addEventListener( 'DOMContentLoaded', function( event ) {
   $(window).on("load", function() {
     $(".header").load("header.html .container > *");
     // $("head > link:nth-child(3)").after('<link rel="stylesheet" href="css/header.css">');
-    $('head > *').attr('href','css/media.css').before('<link rel="stylesheet" href="css/header.css">');
+    $('head > *').attr('href','css/media.css').after('<link rel="stylesheet" href="css/header.css">');
     // $('<link rel="stylesheet" type="text/css" href="css/media.css">').after('<link rel="stylesheet" type="text/css" href="css/header.css">');
 
     $.getScript("js/custom-scrollbar.js");
