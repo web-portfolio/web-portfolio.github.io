0cd0bca4d87de28e07ea6a096b449dcf14412ad6
web-portfolio <nasko.1806@gmail.com>
2019-01-29 22:36:02 +0200

    Update web-mobile.js

--- test/js/web-mobile.js
+++ test/js/web-mobile.js
@@ -32,95 +32,95 @@ function getMobileOperatingSystem() {
     $('link[href="css/hovers.css"]').prop('disabled', false);
     // HEADER
     $(".mob-close").hide();
-    $(".nav-logo").hover(function() {
+    $(".nav-logo").mouseenter(function() {
       $(".nav-logo img").prop("src", "img/nav-logo-hover.png");
-    }, function() {
+    }).mouseleave(function() {
       $(".nav-logo img").prop("src", "img/nav-logo.png");
     });
 
-    $(".games").hover(function() {
+    $(".games").mouseenter(function() {
       $(".buy-game-icon").prop("src", "img/ico/buy-hover.png");
-    }, function() {
+    }).mouseleave(function() {
       $(".buy-game-icon").prop("src", "img/ico/buy.png");
     });
 
-    $(".profile").hover(function() {
+    $(".profile").mouseenter(function() {
       $(".profile-icon").prop("src", "img/ico/profile-hover.png");
-    }, function() {
+    }).mouseleave(function() {
       $(".profile-icon").prop("src", "img/ico/profile.png");
     });
 
-    $(".game").hover(function() {
+    $(".game").mouseenter(function() {
       var hImg = $("img", this).outerHeight();
       var wImg = $("img", this).outerWidth();
       $(".buy-this", this).outerHeight(hImg);
       $(".buy-this", this).outerWidth(wImg);
       $(".buy-this", this).slideDown();
-    }, function() {
+    }).mouseleave(function() {
       $(".buy-this", this).stop(true, false).slideUp();
     });
     // FOOTER
-    $(".back-to-top").hover(function() {
+    $(".back-to-top").mouseenter(function() {
       x = true;
       animate();
-    }, function() {
+    }).mouseleave(function() {
       $(".back-to-top").stop(true, false);
       $(".back-to-top").animate({
         "padding": "0"
       }, 300);
       x = false;
     });
-    $(".footer-phone").hover(function() {
+    $(".footer-phone").mouseenter(function() {
       $(this).css("background-image", "url('img/ico/phone-hover.png')");
-    }, function() {
+    }).mouseleave(function() {
       $(this).css("background-image", "url('img/ico/phone.png')");
     });
 
-    $(".footer-email").hover(function() {
+    $(".footer-email").mouseenter(function() {
       $(this).css("background-image", "url('img/ico/mail-hover.png')");
-    }, function() {
+    }).mouseleave(function() {
       $(this).css("background-image", "url('img/ico/mail.png')");
     });
 
-    $(".live-chat").hover(function() {
+    $(".live-chat").mouseenter(function() {
       $(this).css("background-image", "url('img/ico/chat-hover.png')");
-    }, function() {
+    }).mouseleave(function() {
       $(this).css("background-image", "url('img/ico/chat.png')");
     });
 
-    $(".cookie").hover(function() {
+    $(".cookie").mouseenter(function() {
       $(this).css("background-image", "url('img/ico/cookie-hover.png')");
-    }, function() {
+    }).mouseleave(function() {
       $(this).css("background-image", "url('img/ico/cookie.png')");
     });
 
-    $(".privacy").hover(function() {
+    $(".privacy").mouseenter(function() {
       $(this).css("background-image", "url('img/ico/privacy-hover.png')");
-    }, function() {
+    }).mouseleave(function() {
       $(this).css("background-image", "url('img/ico/privacy.png')");
     });
 
-    $(".f1").hover(function() {
+    $(".f1").mouseenter(function() {
       $(this).css("background-image", "url('img/ico/f1-hover.png')");
-    }, function() {
+    }).mouseleave(function() {
       $(this).css("background-image", "url('img/ico/f1.png')");
     });
 
-    $(".f2").hover(function() {
+    $(".f2").mouseenter(function() {
       $(this).css("background-image", "url('img/ico/f2-hover.png')");
-    }, function() {
+    }).mouseleave(function() {
       $(this).css("background-image", "url('img/ico/f2.png')");
     });
 
-    $(".f3").hover(function() {
+    $(".f3").mouseenter(function() {
       $(this).css("background-image", "url('img/ico/f3-hover.png')");
-    }, function() {
+    }).mouseleave(function() {
       $(this).css("background-image", "url('img/ico/f3.png')");
     });
 
-    $(".f4").hover(function() {
+    $(".f4").mouseenter(function() {
       $(this).css("background-image", "url('img/ico/f4-hover.png')");
-    }, function() {
+    }).mouseleave(function() {
       $(this).css("background-image", "url('img/ico/f4.png')");
     });
 
