efdff04acdea908e5d1c10377e0f6949203d56e5
web-portfolio <nasko.1806@gmail.com>
2019-01-29 23:17:19 +0200

    Update footer.js

--- test/js/footer.js
+++ test/js/footer.js
@@ -84,7 +84,7 @@ $(".f1").click(function() {
   window.location = "https://bg-bg.facebook.com/Warcraft";
 });
 $(".f2").click(function() {
-  window.open = ("https://twitter.com/warcraft?lang=bg", "_blank");
+  window.open("https://twitter.com/warcraft?lang=bg", "_blank");
 });
 $(".f3").click(function() {
   window.location = "https://www.youtube.com/results?search_query=world+of+warcraft+trailers";
