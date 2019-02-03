2e8c434f25c61b61b648708e3cb399c813df1e60
web-portfolio <nasko.1806@gmail.com>
2019-02-03 09:51:20 +0200

    Update home-page.js

--- test/js/home-page.js
+++ test/js/home-page.js
@@ -18,9 +18,10 @@ document.addEventListener( "DOMContentLoaded", function( event ) {
     $.getScript("js/footer.js");
 
     //OS SYSTEM
+    $.getScript("js/web-mobile.js");
+
     $("head .media").before('<link rel="stylesheet" href="css/custom-scrollbar.css">');
     $.getScript("js/custom-scrollbar.js");
-    $.getScript("js/web-mobile.js");
   });
 
   $(".flexslider").flexslider({
