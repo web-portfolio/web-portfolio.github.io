e87c546df75bedff3b0575182daa07d3ea5d33f6
web-portfolio <nasko.1806@gmail.com>
2019-02-18 23:19:37 +0200

    Update config-home-page.js

--- test/js/config-home-page.js
+++ test/js/config-home-page.js
@@ -1,7 +1,7 @@
 requirejs.config({
     "baseUrl": "js/lib",
     "paths": {
-      "config": "../config",
+      "config": "../config-home-page",
       "jquery": "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min",
       // "jquery": "jquery-3.2.1",
       "flexslider": "jquery-flexslider",
