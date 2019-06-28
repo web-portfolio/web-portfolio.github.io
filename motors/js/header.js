af5973f11304d48c69940e185a72f451ca9aba28
web-portfolio <46299764+web-portfolio@users.noreply.github.com>
2019-06-28 15:16:04 +0300

    motors

--- motors/js/header.js
+++ motors/js/header.js
@@ -2,3 +2,8 @@ $(".navigation li").click(function() {
   $(".navigation li").removeClass("nav-active");
   $(this).addClass("nav-active");
 });
+
+// JS LINKS
+$(".nav-products").click(function() {
+  window.location = "products.html";
+});
---
+++ motors/js/products.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  $(window).on("load", function() {
+    loadElements();
+  });
+
+  $(window).on("load resize", function() {
+
+  });
+
+  function loadElements() {
+    $(".header").load("header.html .container > *", function() {
+      $.getScript("js/header.js");
+    });
+    $("head .media").before('<link rel="stylesheet" href="css/header.css">');
+
+    $(".footer").load("footer.html .container > *", function() {
+      $.getScript("js/footer.js");
+      $.getScript("js/web-mobile.js");
+      $(".loader-bg").fadeOut(500);
+    });
+
+    $("head .media").before('<link rel="stylesheet" href="css/footer.css">');
+  };
+});
