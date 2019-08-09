29d92dae636b23caeb2ea1df0e871d1d9b395e98
web-portfolio <nasko.1806@gmail.com>
2019-08-09 11:33:07 +0300

    tuch event

--- efbet-landings/liga/js/liga.js
+++ efbet-landings/liga/js/liga.js
@@ -1,55 +1,64 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  $(window).on("load", function() {
+document.addEventListener("DOMContentLoaded", function (event) {
+  $(window).on("load", function () {
     $(".loader-bg").fadeOut(500);
   });
 
-  $(window).on("load resize", function() {
+  $(window).on("load resize", function () {
     getMobileOperatingSystem();
     fixedLogo();
   });
 
-  $(window).on("resize", function() {
+  $(window).on("resize", function () {
     mobOverfllow();
   });
 
-  setTimeout(function() {
+  setTimeout(function () {
     $(".pop-up").slideDown();
   }, 1500);
 
-  $(".pop-up-btn").click(function() {
+  $(".pop-up-btn").click(function () {
     $(".pop-up").slideUp();
   });
 
-  $(".hamburger").click(function() {
+  $(".hamburger").click(function () {
     $(this).toggleClass("hamburger-active");
     $(this).css("pointer-events", "none");
-    $(".nav-content").slideToggle(function(){
+    $(".nav-content").slideToggle(function () {
       $(".hamburger").css("pointer-events", "all");
     });
+
+    // $(".nav-content").mouseenter(function(){
+    //   $("body").css("overflowY", "hidden");
+    // });
+
     if ($(".hamburger").hasClass("hamburger-active")) {
-      $("body").css("overflowY", "hidden");
+      // $("body").css("overflowY", "hidden");
     } else {
-      $("body").css("overflowY", "auto");
+      // $("body").css("overflowY", "auto");
     }
   });
 
-  $(".open-rules").click(function() {
+  $(".top-img").mouseenter(function(){
+    $("body").css("overflowY", "hidden");
+  });
+
+  $(".open-rules").click(function () {
     $(".rule-text-container").fadeIn(500);
     $("body").css("overflowY", "hidden");
   });
 
-  $(".close-btn").click(function() {
+  $(".close-btn").click(function () {
     $(".rule-text-container").fadeOut(500);
     $("body").css("overflowY", "auto");
   });
 
   function mobOverfllow() {
-    $(".hamburger").removeClass("hamburger-active");
-    $(".nav-content").slideUp();
-    $("body").css("overflowY", "auto");
+    // $(".hamburger").removeClass("hamburger-active");
+    // $(".nav-content").slideUp();
+    // $("body").css("overflowY", "auto");
   }
 
-  $(".rules").click(function() {
+  $(".rules").click(function () {
     $(".nav-content").hide();
     $(".hamburger").removeClass("hamburger-active");
     $("body").css("overflowY", "auto");
@@ -77,25 +86,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
     if (winPhone || ios || android) {
       $('link[href="css/hovers.css"]').prop("disabled", true);
       $("body *").unbind("mouseenter mouseleave");
-      $(".registration").click(function() {
+      $(".registration").click(function () {
         window.location = "https://mobile.efbet.com/register";
       });
-      $(".bet").click(function() {
+      $(".bet").click(function () {
         window.location = "https://mobile.efbet.com/sports/node/361881.1";
       });
-      $(".footer-btn, .winners").click(function() {
+      $(".footer-btn, .winners").click(function () {
         window.location = "https://liga.efbet.com/";
       });
       return "Phone Device";
     } else {
       $('link[href="css/hovers.css"]').prop("disabled", false);
-      $(".registration").click(function() {
+      $(".registration").click(function () {
         window.location = "https://www.efbet.com/BG/Registration#action=register";
       });
-      $(".bet").click(function() {
+      $(".bet").click(function () {
         window.location = "https://www.efbet.com/BG/sports#bo-navigation=282241.1,282242.1,361881.1&action=market-group-list";
       });
-      $(".footer-btn, .winners").click(function() {
+      $(".footer-btn, .winners").click(function () {
         window.location = "https://liga.efbet.com/";
       });
       return "Desktop";
