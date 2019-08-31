d81a3d12d639208cf042d931f536187c566a64b3
web-portfolio <nasko.1806@gmail.com>
2019-08-31 18:55:31 +0300

    test

--- phplogin1/js/index.js
+++ phplogin1/js/index.js
@@ -1,4 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  $(window).on("resize", function() {
+    mobOverfllow();
+  });
+
+  $(".hamburger").click(function() {
+    $(this).toggleClass("hamburger-active");
+    $(this).css("pointer-events", "none");
+    $(".nav-content").slideToggle(function() {
+      $(".hamburger").css("pointer-events", "all");
+    });
+    mobOverfllow();
+  });
+
+  function mobOverfllow() {
+    if ($(".hamburger").hasClass("hamburger-active") && $(".hamburger").height() > 0) {
+      $("body").css("overflowY", "hidden");
+      document.addEventListener('touchmove', touchPreven, { passive: false });
+    } else {
+      $("body").css("overflowY", "auto");
+      document.removeEventListener('touchmove', touchPreven);
+    }
+  };
+
+  function touchPreven(e) {
+    e.preventDefault();
+  }
+
   $(".tab-link-container li").click(function() {
     var data_id = $(this).attr("data-tab");
     $(this).parent().addClass("current-parent");
