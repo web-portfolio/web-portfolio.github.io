82414355311dfb57c73dd3c8909d2e3a9a199a4b
web-portfolio <nasko.1806@gmail.com>
2019-10-04 23:53:38 +0300

    local store

--- wow/js/buy-all.js
+++ wow/js/buy-all.js
@@ -37,14 +37,115 @@ document.addEventListener("DOMContentLoaded", function(event) {
     $(".cover-art-menu").css({ "margin-top": (pcHeight - camHeight) / 2 });
   };
 
-  $(".cover-art-menu li").click(function() {
-    var data_id = $(this).attr("data-tab");
-    $(this).parent().addClass("cover-art-current-parent");
-    $(this).parent().prev().addClass("cover-art-current-parent");
-    $(".cover-art-current-parent .cover-art-tab-link, .cover-art-current-parent .cover-art-tab-content").removeClass("cover-art-current");
+  var bc_cover_art_last_id = localStorage.getItem("bc_cover_art_tab_id");
+  if (bc_cover_art_last_id) {
+    $(".cover-art-menu-bc li").removeClass("cover-art-current");
+    $(".cover-art-bc > div").removeClass("cover-art-current");
+    $(".cover-art-menu-bc li").eq(Number(bc_cover_art_last_id.match(/\d+/)[0]) - 1).addClass("cover-art-current");
+    $("#" + bc_cover_art_last_id).addClass("cover-art-current");
+  }
+  $(".cover-art-menu-bc li").click(function() {
+    var bc_cover_art_tab_id = $(this).attr("data-tab");
+    $(".cover-art-menu-bc li").removeClass("cover-art-current");
+    $(".cover-art-bc > div").removeClass("cover-art-current");
     $(this).addClass("cover-art-current");
-    $(".cover-art-current-parent " + "#" + data_id).addClass("cover-art-current");
-    $(this).parent().removeClass("cover-art-current-parent");
-    $(this).parent().prev().removeClass("cover-art-current-parent");
+    $("#" + bc_cover_art_tab_id).addClass("cover-art-current");
+    localStorage.setItem("bc_cover_art_tab_id", bc_cover_art_tab_id);
+  });
+
+  var wolk_cover_art_last_id = localStorage.getItem("wolk_cover_art_tab_id");
+  if (wolk_cover_art_last_id) {
+    $(".cover-art-menu-wolk li").removeClass("cover-art-current");
+    $(".cover-art-wolk > div").removeClass("cover-art-current");
+    $(".cover-art-menu-wolk li").eq(Number(wolk_cover_art_last_id.match(/\d+/)[0]) - 1).addClass("cover-art-current");
+    $("#" + wolk_cover_art_last_id).addClass("cover-art-current");
+  }
+  $(".cover-art-menu-wolk li").click(function() {
+    var wolk_cover_art_tab_id = $(this).attr("data-tab");
+    $(".cover-art-menu-wolk li").removeClass("cover-art-current");
+    $(".cover-art-wolk > div").removeClass("cover-art-current");
+    $(this).addClass("cover-art-current");
+    $("#" + wolk_cover_art_tab_id).addClass("cover-art-current");
+    localStorage.setItem("wolk_cover_art_tab_id", wolk_cover_art_tab_id);
+  });
+
+  var cata_cover_art_last_id = localStorage.getItem("cata_cover_art_tab_id");
+  if (cata_cover_art_last_id) {
+    $(".cover-art-menu-cata li").removeClass("cover-art-current");
+    $(".cover-art-cata > div").removeClass("cover-art-current");
+    $(".cover-art-menu-cata li").eq(Number(cata_cover_art_last_id.match(/\d+/)[0]) - 1).addClass("cover-art-current");
+    $("#" + cata_cover_art_last_id).addClass("cover-art-current");
+  }
+  $(".cover-art-menu-cata li").click(function() {
+    var cata_cover_art_tab_id = $(this).attr("data-tab");
+    $(".cover-art-menu-cata li").removeClass("cover-art-current");
+    $(".cover-art-cata > div").removeClass("cover-art-current");
+    $(this).addClass("cover-art-current");
+    $("#" + cata_cover_art_tab_id).addClass("cover-art-current");
+    localStorage.setItem("cata_cover_art_tab_id", cata_cover_art_tab_id);
+  });
+
+  var pandaria_cover_art_last_id = localStorage.getItem("pandaria_cover_art_tab_id");
+  if (pandaria_cover_art_last_id) {
+    $(".cover-art-menu-pandaria li").removeClass("cover-art-current");
+    $(".cover-art-pandaria > div").removeClass("cover-art-current");
+    $(".cover-art-menu-pandaria li").eq(Number(pandaria_cover_art_last_id.match(/\d+/)[0]) - 1).addClass("cover-art-current");
+    $("#" + pandaria_cover_art_last_id).addClass("cover-art-current");
+  }
+  $(".cover-art-menu-pandaria li").click(function() {
+    var pandaria_cover_art_tab_id = $(this).attr("data-tab");
+    $(".cover-art-menu-pandaria li").removeClass("cover-art-current");
+    $(".cover-art-pandaria > div").removeClass("cover-art-current");
+    $(this).addClass("cover-art-current");
+    $("#" + pandaria_cover_art_tab_id).addClass("cover-art-current");
+    localStorage.setItem("pandaria_cover_art_tab_id", pandaria_cover_art_tab_id);
+  });
+
+  var wod_cover_art_last_id = localStorage.getItem("wod_cover_art_tab_id");
+  if (wod_cover_art_last_id) {
+    $(".cover-art-menu-wod li").removeClass("cover-art-current");
+    $(".cover-art-wod > div").removeClass("cover-art-current");
+    $(".cover-art-menu-wod li").eq(Number(wod_cover_art_last_id.match(/\d+/)[0]) - 1).addClass("cover-art-current");
+    $("#" + wod_cover_art_last_id).addClass("cover-art-current");
+  }
+  $(".cover-art-menu-wod li").click(function() {
+    var wod_cover_art_tab_id = $(this).attr("data-tab");
+    $(".cover-art-menu-wod li").removeClass("cover-art-current");
+    $(".cover-art-wod > div").removeClass("cover-art-current");
+    $(this).addClass("cover-art-current");
+    $("#" + wod_cover_art_tab_id).addClass("cover-art-current");
+    localStorage.setItem("wod_cover_art_tab_id", wod_cover_art_tab_id);
+  });
+
+  var legion_cover_art_last_id = localStorage.getItem("legion_cover_art_tab_id");
+  if (legion_cover_art_last_id) {
+    $(".cover-art-menu-legion li").removeClass("cover-art-current");
+    $(".cover-art-legion > div").removeClass("cover-art-current");
+    $(".cover-art-menu-legion li").eq(Number(legion_cover_art_last_id.match(/\d+/)[0]) - 1).addClass("cover-art-current");
+    $("#" + legion_cover_art_last_id).addClass("cover-art-current");
+  }
+  $(".cover-art-menu-legion li").click(function() {
+    var legion_cover_art_tab_id = $(this).attr("data-tab");
+    $(".cover-art-menu-legion li").removeClass("cover-art-current");
+    $(".cover-art-legion > div").removeClass("cover-art-current");
+    $(this).addClass("cover-art-current");
+    $("#" + legion_cover_art_tab_id).addClass("cover-art-current");
+    localStorage.setItem("legion_cover_art_tab_id", legion_cover_art_tab_id);
+  });
+
+  var bfa_cover_art_last_id = localStorage.getItem("bfa_cover_art_tab_id");
+  if (bfa_cover_art_last_id) {
+    $(".cover-art-menu-bfa li").removeClass("cover-art-current");
+    $(".cover-art-bfa > div").removeClass("cover-art-current");
+    $(".cover-art-menu-bfa li").eq(Number(bfa_cover_art_last_id.match(/\d+/)[0]) - 1).addClass("cover-art-current");
+    $("#" + bfa_cover_art_last_id).addClass("cover-art-current");
+  }
+  $(".cover-art-menu-bfa li").click(function() {
+    var bfa_cover_art_tab_id = $(this).attr("data-tab");
+    $(".cover-art-menu-bfa li").removeClass("cover-art-current");
+    $(".cover-art-bfa > div").removeClass("cover-art-current");
+    $(this).addClass("cover-art-current");
+    $("#" + bfa_cover_art_tab_id).addClass("cover-art-current");
+    localStorage.setItem("bfa_cover_art_tab_id", bfa_cover_art_tab_id);
   });
 });
