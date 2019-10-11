c7c1c52694a02ca46b02d2483549aef8c4cb0861
web-portfolio <nasko.1806@gmail.com>
2019-10-11 18:00:15 +0300

    test

---
+++ test/js.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  mapboxgl.accessToken = "pk.eyJ1IjoibmFza285MCIsImEiOiJjazFqMHB5bXgwc3VtM3BqeWJ3Z2F6N2p1In0.UIeRf1hLbffdYk6bibVbLg";
+  // mapboxgl.accessToken = "pk.eyJ1IjoibmFza285MCIsImEiOiJjazFrdjhucDQwMDc3M2JsanhocjE5em9sIn0.LOBSHBPTHrQnuWOd7NbKMg";  
+  var map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [23.325691223144528, 42.69795509187187],
+    zoom: 13
+  });
+  // map vision
+  var layerList = document.getElementById('menu');
+  var inputs = layerList.getElementsByTagName('input');
+
+  function switchLayer(layer) {
+    var layerId = layer.target.id;
+    map.setStyle('mapbox://styles/mapbox/' + layerId);
+  }
+
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].onclick = switchLayer;
+  }
+
+
+  // za full screen
+  // butoni za zoom i vurtene
+  // custom icons
+});
