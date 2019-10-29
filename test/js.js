d81d76b0dda522197119489625c950677c2bfb2d
web-portfolio <nasko.1806@gmail.com>
2019-10-29 16:49:51 +0200

    test

--- test/js.js
+++ test/js.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   // $(window).on("load", function() {
-  //   $("#nav-menu a,a[href='#top'],a[rel='m_PageScroll2id']").mPageScroll2id({
+  //   $("#nav-menu a,a[href="#top"],a[rel="m_PageScroll2id"]").mPageScroll2id({
   //     highlightSelector: "#nav-menu a"
   //   });
 
-  //   $("a[rel='next']").click(function(e) {
+  //   $("a[rel="next"]").click(function(e) {
   //     e.preventDefault();
   //     var to = $(this).parent().parent("section").next().attr("id");
   //     $.mPageScroll2id("scrollTo", to);
   //   });
   // });
 
-  $("#nav-menu ul li").click(function() {
+  $("#nav-menu li").click(function() {
     var tab_id = $(this).attr("data-tab");
-    $("#nav-menu ul li").removeClass("current");
+    $("#nav-menu li").removeClass("current");
     $(".tab-content").removeClass("current-scroll");
     $(this).addClass("current");
     $("#" + tab_id).addClass("current-scroll");
@@ -21,14 +21,35 @@ document.addEventListener("DOMContentLoaded", function(event) {
       scrollTop: $(".current-scroll").offset().top
     }, 500);
   });
-  $('.tab-content').each(function() {
-    var x = ($(this).offset().top);
-    console.log(x);
-  });
-  $(window).scroll(function() {
-    var winTop = $(window).scrollTop();
-    var winHalf = $(window).height() / 2;
-    console.log("window= " + (winTop + winHalf));
 
+  // $(".tab-content").each(function() {
+  //   var x = ($(this).offset().top);
+  //   console.log(x);
+  // });
+  // $(window).scroll(function() {
+  //   var winTop = $(window).scrollTop();
+  //   var winHalf = $(window).height() / 2;
+  //   console.log("window= " + (winTop + winHalf));
+  // });
+
+  $.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+  };
+
+  $(window).on("resize scroll", function() {
+    $(".tab-content").each(function() {
+      var elementId = $(this).attr("id");
+      var tabData = $(".tab-link[data-tab=" + elementId + "]");
+      if ($(this).isInViewport()) {
+        tabData.addClass("current")
+      } else {
+        tabData.removeClass("current")
+      }
+    });
   });
+
 });
