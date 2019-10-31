e9979c84024678558e10fef3e946869fe06c3d9d
web-portfolio <nasko.1806@gmail.com>
2019-10-31 23:25:44 +0200

    test

--- nav-menu-map/js.js
+++ nav-menu-map/js.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  $(window).on("load", function() {
+    chnageBackground()
+  });
+
   $(".tab-link").click(function() {
     var tab_id = $(this).attr("data-tab");
     $(".tab-content").removeClass("current-scroll");
@@ -29,11 +33,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
   function chnageBackground() {
-    var img_array = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+    var img_array = ["1.jpg", "2.jpg"],
+      currentIndex = 0,
+      nextIndex = 1,
+      fadeTime = 1000,
+      interval = 2000,
+      element;
 
     function assignBackgrounds() {
       for (var i = 0; i < img_array.length; i++) {
-        $("#slider-web").append("<div class='slider-bg-" + i + "'></div>");
+        $(".slider-bgs").append("<div class='slider-bg-" + i + "'></div>");
         $(".slider-bg-" + i).css("background-image", "url(" + img_array[i] + ")");
         if (i == 0) {
           $(".slider-bg-" + i).show();
@@ -43,109 +52,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
     }
 
-    function changeOpacity() {
-      var element = $(".slider-bg-0"),
-        firstIndex = 0,
-        secondIndex = firstIndex + 1;
-      element.fadeOut(1000);
-      $(".slider-bg-" + secondIndex).fadeIn(1000);
+    function changeVisibility() {
+      setInterval(function() {
+        element = $(".slider-bg-" + currentIndex);
+        element.fadeOut(fadeTime);
+        $(".slider-bg-" + nextIndex).fadeIn(fadeTime);
+        currentIndex = nextIndex;
+        nextIndex = (nextIndex + 1) % img_array.length;
+      }, interval)
     }
 
     assignBackgrounds();
-    changeOpacity();
+    changeVisibility();
   }
-
-
-
-
-
-
-
-
-  // function startBackgroundOpacityToggle() {
-  //   elem = $('#background-slide-web' + _curIndex);
-  //   elem.animate({
-  //     opacity: (elem.css('opacity') == 0) ? 1 : 0
-  //   }, {
-  //     duration: 1000,
-  //     start: finishBackgroundOpacityToggle
-  //   });
-  // };
-
-
-  $(window).on("load", function() {
-    // changeBgWeb();
-    chnageBackground()
-  });
-
-  function changeBgWeb() {
-    var imgCount = 0;
-    var img_array = ["1.jpg", "2.jpg"],
-      _nxtIndex = 0,
-      _curIndex = 0,
-      interval = 2000;
-
-    function nextIndex() {
-      _nxtIndex = (_nxtIndex + 1) % img_array.length;
-      return _nxtIndex;
-    };
-
-    function shiftIndexes() {
-      _curIndex = _nxtIndex;
-      nextIndex();
-    }
-
-    function createImgTags() {
-      imgCount = img_array.length;
-      var html = '';
-      var slider = document.getElementById('slider-web');
-      for (var i = 0; i < imgCount; i++) {
-        html += '<div id="background-slide-web' + i + '" class="background-slider-web"></div>';
-      }
-      $(slider).html(html);
-    }
-
-    function assignBackgrounds() {
-      imgCount = img_array.length;
-      for (var i = 0; i < imgCount; i++) {
-
-        $('#background-slide-web' + i).css('backgroundImage', function() {
-          return 'url(' + img_array[nextIndex()] + ')';
-        });
-        if (i == 0) {
-          $('#background-slide-web' + i).css('opacity', 1);
-        } else {
-          $('#background-slide-web' + i).css('opacity', 0);
-        }
-      }
-    }
-
-    function startBackgroundOpacityToggle() {
-      elem = $('#background-slide-web' + _curIndex);
-      elem.animate({
-        opacity: (elem.css('opacity') == 0) ? 1 : 0
-      }, {
-        duration: 1000,
-        start: finishBackgroundOpacityToggle
-      });
-    };
-
-    function finishBackgroundOpacityToggle() {
-      elem = $('#background-slide-web' + _nxtIndex);
-      elem.animate({
-        opacity: (elem.css('opacity') == 0) ? 1 : 0
-      }, {
-        duration: 1000,
-        complete: runSlider
-      });
-    };
-
-    function runSlider() {
-      shiftIndexes();
-      setTimeout(startBackgroundOpacityToggle, interval);
-    }
-    createImgTags();
-    assignBackgrounds();
-    runSlider();
-  };
 });
