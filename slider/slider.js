a901e706e0c7fc1aee6aa9e7126958f4d46c16fd
web-portfolio <nasko.1806@gmail.com>
2019-11-23 23:29:09 +0200

    slider test

---
+++ slider/slider.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var sliderWidth = $(".slider").width(),
+    sliderHeight = $(".slider").height(),
+    numOfSlides = $(".slider-content > div").length,
+    interval = 5000,
+    slideInterval = 500;
+
+  $(".slider-content").width(numOfSlides * sliderWidth).css("left", -sliderWidth);
+  $(".slider-content > div").width(sliderWidth).height(sliderHeight);
+  $(".slider-content div:last-child").prependTo($(".slider-content"));
+
+  function moveLeft() {
+    $(".slider-content").animate({
+      left: -sliderWidth * 2
+    }, slideInterval, function() {
+      $(".slider-content div:first-child").appendTo($(".slider-content"));
+      $(".slider-content").css("left", -sliderWidth);
+    });
+  };
+
+  function moveRight() {
+    $(".slider-content").animate({
+      left: 0
+    }, slideInterval, function() {
+      $(".slider-content div:last-child").prependTo($(".slider-content"));
+      $(".slider-content").css("left", -sliderWidth);
+    });
+  };
+
+  var autoSlide = setInterval(moveLeft, interval)
+
+  $(".slider").mouseenter(function() {
+    clearInterval(autoSlide)
+  }).mouseleave(function() {
+    autoSlide = setInterval(moveLeft, interval)
+  })
+
+
+
+
+  // console.log(numOfSlides)
+});
