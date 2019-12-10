967b36f2309caee9c152c01792fda6efcb774022
web-portfolio <nasko.1806@gmail.com>
2019-12-10 12:14:01 +0200

    test

--- games_reorder/js/games_reorder.js
+++ games_reorder/js/games_reorder.js
@@ -1,14 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  $(window).on("load", function() {
+    $(".games_count").text(game_id.length);
+  });
+
+  var game_id = $(".to_be_converted GamesContainer Game");
+  for (var i = 1; i <= game_id.length; i++) {
+    var text_id = $(".to_be_converted GamesContainer Game:nth-child(" + i + ") GameID").text();
+    $(".to_be_converted GamesContainer Game:nth-child(" + i + ")").addClass(text_id)
+  }
+
   $(".split_game_names").click(function() {
-    var game_array = $(".game_names").val().split(";");
+    var game_array = $(".game_names").val().split("\n");
 
     for (var i = 0; i <= game_array.length; i++) {
-      $("#" + game_array[i]).appendTo(".ready .game_container");
+      $("." + game_array[i]).appendTo(".ready GamesContainer");
     }
+    $(".games_count").text(game_id.length - game_array.length);
+    $(".games_count_ready").text(game_array.length);
   })
 
-  $(".btn_tenst").click(function() {
-    download("game_order.html", $(".ready").html());
+  $(".export").click(function() {
+    $(".ready GamesContainer Game").removeAttr("class");
+    download("game_order_com.html", $(".ready .games").html());
   });
 
   function download(filename, text) {
